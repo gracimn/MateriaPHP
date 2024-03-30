68361a5f27e8362f638a273ee555c4e98c8a04b5
Gra <gracielle.gfo.oliveira@gmail.com>
2024-03-30 00:29:06 -0300

    Exercícios de PHP do 1 a 10

---
+++ ExerciciosPHP/src/view/6.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 6</title>
+</head>
+<body>
+    <h1>Ordem crescente de valores</h1>
+    <form action="" method="POST">
+        <label>Digite o 1° numero: </label><input type="number" name="n1"><br>
+        <label>Digite o 2° número: </label><input type="number" name="n2"><br>
+        <input type="submit">
+    </form> 
+</body>
+</html>
