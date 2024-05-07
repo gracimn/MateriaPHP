aa06cec0458269e24b22a2149b78f1ff5da7b55e
gracimn <102486372+gracimn@users.noreply.github.com>
2024-05-06 22:48:34 -0300

    Exercícios do 1 ao 10
    Adicionados novamente

---
+++ ExerciciosPHP/src/view/1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 1</title>
+</head>
+<body>
+    <h1>Número positivo, negativo ou igual a 0</h1>
+    <form action="" method="POST">
+        <input type="number" name="n1">
+        <input type="submit">
+    </form> 
+</body>
+</html>
---
+++ ExerciciosPHP/src/view/3.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 3</title>
+</head>
+<body>
+    <h1>Soma de 2 números:</h1>
+    <form action="" method="POST">
+        <label>Digite o 1° número: </label><input type="number" name="n1"><br>
+        <label>Digite o 2° número: </label><input type="number" name="n2"><br>
+        <input type="submit">
+    </form> 
+</body>
+</html>
